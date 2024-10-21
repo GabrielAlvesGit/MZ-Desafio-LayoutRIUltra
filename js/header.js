2ceefa4cc0032ce840c6cb1f8d826e3d81e0f8cb
Gabriel Alves <102634725+GabrielAlvesGit@users.noreply.github.com>
2024-10-20 23:50:52 -0300

    mobile menu responsiveness
    mobile menu responsiveness

---
+++ js/header.js
@@ -0,0 +1,18 @@
+/* ============ Header ============ */
+/* ============ Menu ============ */
+var menu = document.querySelector('.header__bottom__navigation');
+var headerBottomToggle = document.querySelector('.header__bottom__toogle__menu');
+var closeIcon = document.querySelector('.header__bottom__toogle__close');
+var headerTop= document.querySelector('.header__top');
+
+
+function toggleMenu() {
+
+  menu.classList.toggle("active");
+  headerBottomToggle.classList.toggle("active");
+  closeIcon.classList.toggle("active");
+  headerTop.classList.toggle("active");
+}
+
+headerBottomToggle.addEventListener('click', toggleMenu);
+closeIcon.addEventListener('click', toggleMenu);
